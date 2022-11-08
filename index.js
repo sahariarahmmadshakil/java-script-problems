d58e39bfefccbdfdf5212439910974cd77a30f6a
sahariarahmmad <info.sashakil@gmail.com>
2022-11-09 01:00:57 +0600

    added array

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let arr =[1,2,3,4,5,6];
-console.log(arr);
+// let numbers = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+// for (let i = 0; i <= numbers.length; i++) {
+//     let number = numbers[i];
+//     if (number >50){
+//             break;
+//         }
+//          console.log(number);
+// }
+let fruits=['apple', 'banana','orange'];
+console.log("Index of banana is:",fruits.indexOf('banana'));
+console.log(fruits);
+fruits.pop()
+console.log(fruits);
